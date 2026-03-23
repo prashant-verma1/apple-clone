872e9d3697472d299d686c69af4064a9d944b4a0
[Prashant Verma] <diamandg5726@gmail.com>
2026-03-24 02:04:46 +0530

    Update components

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,11 @@ import Hero from './components/hero'
 import ProductViewer from './components/ProductViewer'
 import gsap from 'gsap'
 import { ScrollTrigger, SplitText } from 'gsap/all'
+import Showcase from './components/Showcase.jsx'
+import Performance from './components/Performance.jsx'
+import Features from './components/Features.jsx'
+import Highlights from './components/Highlights.jsx'
+import Footer from './components/Footer.jsx'
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
@@ -13,8 +18,13 @@ const App = () => {
     <Navbar />
     <Hero />
     <ProductViewer />
+    <Showcase />
+    <Performance />
+    <Features />
+    <Highlights />
+    <Footer />
     </>
   )
 }
 
-export default App
+export default App;
